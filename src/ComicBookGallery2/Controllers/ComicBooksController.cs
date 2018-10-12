acf9e53ba4db1a95372eecbdb6affcaf23927224
Philip <yaoxs@guotong.org>
2018-10-12 11:14:02 +0800

    Added comic books controller

---
+++ src/ComicBookGallery2/Controllers/ComicBooksController.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ComicBookGallery2.Controllers
+{
+    public class ComicBooksController
+    {
+
+    }
+}
