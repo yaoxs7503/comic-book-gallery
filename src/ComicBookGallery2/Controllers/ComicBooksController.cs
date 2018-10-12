51ece7cae4f535278d375b1955a7a0b9aa5c975b
Philip <yaoxs@guotong.org>
2018-10-12 11:35:47 +0800

    Added Detail action method

--- src/ComicBookGallery2/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery2/Controllers/ComicBooksController.cs
@@ -8,5 +8,9 @@ namespace ComicBookGallery2.Controllers
 {
     public class ComicBooksController : Controller
     {
+        public string Detail()
+        {
+            return "Hello from the comic books controller!";
+        }
     }
 }
